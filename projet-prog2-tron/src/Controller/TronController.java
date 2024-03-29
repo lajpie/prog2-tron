e65e52455d7b7815cdb54b5e5f902be181dfe347
lajpie <78029332+lajpie@users.noreply.github.com>
2024-03-29 16:17:22 -0400

    restart and return to menu

--- projet-prog2-tron/src/Controller/TronController.java
+++ projet-prog2-tron/src/Controller/TronController.java
@@ -5,7 +5,7 @@ import View.TronGame;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.Timer;
+import javax.swing.*;
 
 public class TronController implements ActionListener {
 
@@ -14,6 +14,7 @@ public class TronController implements ActionListener {
 
     //logic
     Timer gameLoop;
+    Object[] endOfGameOptions = {"yes", "no"};
 
     public TronController(TronGame view, MainMenu menuView){
         gameView = view;
@@ -34,6 +35,26 @@ public class TronController implements ActionListener {
             gameLoop.stop();
             System.out.println("game over");
 
+            int choice = JOptionPane.showOptionDialog(
+                    null,
+                    "Play Again?",
+                    "GAME OVER",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    endOfGameOptions,
+                    endOfGameOptions[1]
+            );
+
+            if(choice==JOptionPane.YES_OPTION){
+                gameView.restartGame();
+                startGame();
+            } else {
+                gameView.restartGame();
+                gameView.setVisible(false);
+                menuView.setVisible(true);
+                menuView.requestFocus();
+            }
         }
 
     }
