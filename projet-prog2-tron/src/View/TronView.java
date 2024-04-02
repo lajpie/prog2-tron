55068e6ec4b16ffd7c6e2e151bf500dcfa06388e
lajpie <78029332+lajpie@users.noreply.github.com>
2024-04-02 19:47:52 -0400

    display lives

--- projet-prog2-tron/src/View/TronView.java
+++ projet-prog2-tron/src/View/TronView.java
@@ -33,24 +33,27 @@ public class TronView extends JPanel {
             g.drawLine(0, i * tronGame.getTileSize(), tronGame.getGridWidth(), i*tronGame.getTileSize());
         }
 
+
+
         //murs
         g.setColor(Color.GRAY);
         tronGame.getMurs().forEach((m) -> g.fillRect(m.getX() * tronGame.getTileSize(), m.getY() * tronGame.getTileSize(), tronGame.getTileSize(),tronGame.getTileSize()));
 
-
+        g.setFont(new Font("Arial",Font.BOLD,20));
 
         //player 1
         g.setColor(tronGame.getPlayer1().getColor());
         tronGame.getPlayer1Trail().forEach((m) -> g.fillRect(m.getX() * tronGame.getTileSize(), m.getY() * tronGame.getTileSize(), tronGame.getTileSize(),tronGame.getTileSize()));
         g.setColor(tronGame.getPlayer1().getTrailColor());
         g.fillRect(tronGame.getPlayer1().getX() * tronGame.getTileSize(), tronGame.getPlayer1().getY() * tronGame.getTileSize(), tronGame.getTileSize(),tronGame.getTileSize());
+        g.drawString("Player 1    Lives:"+tronGame.getPlayer1().getLives(), tronGame.getTileSize(),tronGame.getTileSize() -5);
 
         //player 2
         g.setColor(tronGame.getPlayer2().getColor());
         tronGame.getPlayer2Trail().forEach((m) -> g.fillRect(m.getX() * tronGame.getTileSize(), m.getY() * tronGame.getTileSize(), tronGame.getTileSize(),tronGame.getTileSize()));
         g.setColor(tronGame.getPlayer2().getTrailColor());
         g.fillRect(tronGame.getPlayer2().getX() * tronGame.getTileSize(), tronGame.getPlayer2().getY() * tronGame.getTileSize(), tronGame.getTileSize(),tronGame.getTileSize());
-
+        g.drawString("Player 2    Lives:"+tronGame.getPlayer2().getLives(),tronGame.getGridWidth()- tronGame.getTileSize()*8,tronGame.getGridHeight()-5);
 
 
     }
